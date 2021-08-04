8047833401d151738eb7224952f833a1cb93c435
egroenendijk <86627465+egroenendijk@users.noreply.github.com>
2021-08-04 16:35:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,102 @@
+// Homework week 4 Feature #1
+
+let now = new Date();
+
+let currentDate = document.querySelector(".currentDate");
+let currentTime = document.querySelector(".currentTime");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+let month = months[now.getMonth()];
+
+currentDate.innerHTML = `${day} ${month} ${date}`;
+currentTime.innerHTML = `${hours}: ${minutes}`;
+
+// Homework week 4 Feature #2
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#search-input");
+  cityElement.innerHTML = cityInput.value;
+
+  let apiKey = "ff48e8f1972c30f87339cf84950e7d10";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${unit}`;
+
+  axios.get(apiUrl).then(weatherCondition);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+// Homework week 5 search engine
+
+function weatherCondition(response) {
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+
+  let description = document.querySelector("#temperature-description");
+  description.innerHTML = response.data.weather[0].description;
+
+  let percipitation = document.querySelector("#percipitation");
+  percipitation.innerHTML = Math.round(response.data.main.humidity);
+
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+
+  let apiKey = "ff48e8f1972c30f87339cf84950e7d10";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+  axios.get(apiUrl).then(weatherCondition);
+}
+
+// Homework week 5 current location
+
+function locatePosition(position) {
+  let longitude = position.coords.longitude;
+  let latitude = position.coords.latitude;
+
+  let apiKey = "ff48e8f1972c30f87339cf84950e7d10";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+
+  axios.get(apiUrl).then(weatherCondition);
+}
+
+function findLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(locatePosition);
+}
+
+let currentButton = document.querySelector(".current-button");
+currentButton.addEventListener("click", findLocation);
