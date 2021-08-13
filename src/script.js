8f5158e5a5f9a1043a769a039b3dbbfed031232e
egroenendijk <86627465+egroenendijk@users.noreply.github.com>
2021-08-13 11:26:33 +0200

    Update weather icon URL

--- src/script.js
+++ src/script.js
@@ -76,7 +76,7 @@ function weatherCondition(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x/png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
